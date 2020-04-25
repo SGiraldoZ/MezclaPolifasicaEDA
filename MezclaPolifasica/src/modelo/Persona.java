af495ec52d7284159d2f46b86f7f03d3e1aeda27
sgz <sgz@sgz-X455LJ>
2020-04-25 13:29:58 -0500

    Comparte to y get cedula

--- MezclaPolifasica/src/modelo/Persona.java
+++ MezclaPolifasica/src/modelo/Persona.java
@@ -8,6 +8,9 @@ public class Persona implements Comparable<Persona>, Serializable{
 	private String cedula;
 	
 	
+	public String getCedula() {
+		return this.cedula;
+	}
 	
 	public Persona(String nombre, int edad, String cedula) {
 		this.nombre = nombre;
@@ -23,9 +26,11 @@ public class Persona implements Comparable<Persona>, Serializable{
 	
 	@Override
 	public int compareTo(Persona p){
-		int compNombres = this.nombre.compareTo(p.getNombre());
-		/*if (compNombres != 0)*/ return compNombres;
-		//return this.edad-p.getEdad();
+		int comp = this.cedula.compareTo(p.getCedula());
+		if (comp != 0) return comp;
+		comp = this.nombre.compareTo(p.getNombre());
+		if (comp!=0) return comp;
+		return this.edad-p.getEdad();
 	}
 	
 	public String getNombre() {
@@ -41,6 +46,7 @@ public class Persona implements Comparable<Persona>, Serializable{
 		this.edad = edad;
 	}
 	
-	
+	public static void main(String[] args) {
+	}
 
 }

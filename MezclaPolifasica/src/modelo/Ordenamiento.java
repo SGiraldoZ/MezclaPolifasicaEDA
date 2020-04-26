62d752087526f865b60ebc2bc38758f2fdea67ff
sgz <sgz@sgz-X455LJ>
2020-04-25 19:09:26 -0500

    Primer intento de mezcla directa

--- MezclaPolifasica/src/modelo/Ordenamiento.java
+++ MezclaPolifasica/src/modelo/Ordenamiento.java
@@ -1,5 +1,112 @@
 package modelo;
 
-public class Ordenamiento {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import manejoFicheros.*;
+
+public class Ordenamiento {
+	private static String aux1 = "aux1.txt";
+	private static String aux2 = "aux2.txt";
+
+	public static void sortIntsTxt0(String url) throws IOException {
+		(new File(aux1)).createNewFile();
+		(new File(aux2)).createNewFile();
+
+		FileReader frMain = new FileReader(url), frAux1 = new FileReader(Ordenamiento.aux1),
+				frAux2 = new FileReader(aux2);
+		FileWriter fwAux1 = new FileWriter(Ordenamiento.aux1), fwAux2 = new FileWriter(aux2),
+				fwMain = new FileWriter(url);
+		BufferedReader brMain = new BufferedReader(frMain), brAux1 = new BufferedReader(frAux1),
+				brAux2 = new BufferedReader(frAux2);
+		BufferedWriter bwMain = new BufferedWriter(fwMain), bwAux1 = new BufferedWriter(fwAux1),
+				bwAux2 = new BufferedWriter(fwAux2);
+
+		int tot = 0;
+		int div = 0;
+		int lenSecuencia = 1;
+		int eDivid; // Numeros de elementos asignados en una fase de división
+		int numMezclados,nM1, nM2;
+		boolean aux1 = true;
+		String line, line1, line2;
+		do {
+			frMain = new FileReader(url);
+			brMain = new BufferedReader(frMain);
+			fwAux1 = new FileWriter(Ordenamiento.aux1);
+			fwAux2 = new FileWriter(aux2);
+			bwAux1 = new BufferedWriter(fwAux1);
+			bwAux2 = new BufferedWriter(fwAux2);
+
+			eDivid = 0;
+			// Se hace la division numero div
+			while ((line = brMain.readLine()) != null) {
+				if (aux1) {
+					bwAux1.write(line);
+				} else {
+					bwAux2.write(line);
+				}
+				eDivid++;
+				aux1 = (eDivid % lenSecuencia == 0) ? !aux1 : aux1;
+				if (div == 0)
+					tot++;
+			}
+			div++;
+			frMain.close();
+			brMain.close();
+			fwAux1.close();
+			fwAux2.close();
+			bwAux1.close();
+			bwAux2.close();
+
+			// FASE MEZCLA
+
+			lenSecuencia = (int) Math.pow(2, div);
+			numMezclados = 0;
+			frAux1 = new FileReader(Ordenamiento.aux1);
+			frAux2 = new FileReader(aux2);
+			fwMain = new FileWriter(url);
+			brAux1 = new BufferedReader(frAux1);
+			brAux2 = new BufferedReader(frAux2);
+			bwMain = new BufferedWriter(fwMain);
+			line1 = brAux1.readLine();
+			line2 = brAux2.readLine();
+			nM1 = 0;
+			nM2 = 0;
+			while (numMezclados < tot) {
+				while (nM1 + nM2 < lenSecuencia) {
+					if (line1.compareTo(line2) < 0 && nM1 < lenSecuencia / 2) {
+						bwMain.write(line1);
+						line1 = brAux1.readLine();
+						nM1++;
+					} else if(nM2 < lenSecuencia/2){
+						bwMain.write(line2);
+						line2 = brAux2.readLine();
+						nM2++;
+					}
+				}
+				bwMain.flush();
+			}
+
+			frAux1.close();
+			frAux2.close();
+			fwMain.close();
+			brAux1.close();
+			brAux2.close();
+			bwMain.close();
+
+		} while (Math.pow(2, div) < tot);
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			sortIntsTxt0("naturales.txt");
+		} catch (IOException e) {
+			System.out.print(e.getMessage());
+		}
+	}
 }
